8d6233923c1f378383e5976e45e07f21cbbe9185
Али Умаров <ali_umarov@Alis-MacBook-Pro.local>
2025-11-26 20:47:16 +0300

    добавил слой репозиторий для жанров

---
+++ internal/repository/genre_repository.go
@@ -0,0 +1,46 @@
+package repository
+
+import (
+	"book-store/internal/models"
+
+	"gorm.io/gorm"
+)
+
+type GenreRepository interface {
+	Create(genre *models.Genre) error
+	GetGenres() ([]models.Genre, error)
+	Update(genre *models.Genre) error
+	Delete(id uint) error
+}
+
+type gormGenreRepository struct {
+	db *gorm.DB
+}
+
+func NewGenreRepository(db *gorm.DB) GenreRepository {
+	return &gormGenreRepository{db: db}
+}
+
+func (r *gormGenreRepository) Create(genre *models.Genre) error {
+	return r.db.Create(genre).Error
+}
+
+func (r *gormGenreRepository) GetGenres() ([]models.Genre, error) {
+	var genres []models.Genre
+
+	err := r.db.Find(genres).Error
+
+	if err != nil {
+		return nil, err
+	}
+
+	return genres, err
+}
+
+func (r *gormGenreRepository) Update(genre *models.Genre) error {
+	return r.db.Updates(&genre).Error
+}
+
+func (r *gormGenreRepository) Delete(id uint) error {
+	return r.db.Delete(id).Error
+}
